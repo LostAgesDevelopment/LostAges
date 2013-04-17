64c15d559c3fb06926b0bbec6ca961a91f8cf2ef
Nick Mirasol <takutastrong@yahoo.com>
2013-04-17 10:17:04 -0500

    Fixed iconIndex

--- common/com/lostages/item/ItemLostAgesShovel.java
+++ common/com/lostages/item/ItemLostAgesShovel.java
@@ -21,7 +21,7 @@ public class ItemLostAgesShovel extends ItemSpade {
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister iconRegister) 
 	{
-		itemIcon = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+		iconIndex = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
 	}
 
 }

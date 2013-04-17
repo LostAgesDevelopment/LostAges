16491be4435a162da7aa0dcbec1770b6e06326cc
Nick Mirasol <takutastrong@yahoo.com>
2013-04-17 10:43:14 -0500

    Added functions to constructor

--- common/com/lostages/world/BiomeWasteland.java
+++ common/com/lostages/world/BiomeWasteland.java
@@ -11,6 +11,10 @@ public class BiomeWasteland extends BiomeGenBase
 		super(id);
 		topBlock = (byte) Block.grass.blockID;
 		fillerBlock = (byte) Block.dirt.blockID;
+		setColor(16421912);
+		setBiomeName("Wasteland").setDisableRain();
+		setTemperatureRainfall(2.0F, 0.0F);
+		setMinMaxHeight(0.1F, 0.2F);
 	}
 
 }

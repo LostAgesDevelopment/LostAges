aedbd0783e21373034bc59968b32b2d129eaad36
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-04-21 03:07:07 +0200

    Changed ID system to make it easier to add already used IDs

--- common/com/lostages/lib/ItemIDs.java
+++ common/com/lostages/lib/ItemIDs.java
@@ -2,21 +2,36 @@ package com.lostages.lib;
 
 public class ItemIDs
 {
-
 	//Default id that is written to configuration file
 	//Default id must be above 3850
 	public static int BONE_INGOT_DEFAULT = 4000;
 	public static int ADAMANT_INGOT_DEFAULT = 4001;
 	
-	public static int SWORD_BONE_DEFAULT = 5000;
-	public static int PICKAXE_BONE_DEFAULT = 5001;
-	public static int SHOVEL_BONE_DEFAULT = 5002;
-	public static int AXE_BONE_DEFAULT = 5003;
+	public static int BONE_DEFAULT = 5000;
+	public static int SWORD_BONE_DEFAULT = BONE_DEFAULT;
+	public static int PICKAXE_BONE_DEFAULT = BONE_DEFAULT + 1;
+	public static int SHOVEL_BONE_DEFAULT = BONE_DEFAULT + 2;
+	public static int AXE_BONE_DEFAULT = BONE_DEFAULT + 3;
+	public static int HAMMER_BONE_DEFAULT = BONE_DEFAULT + 4;
+	//public static int POSSIBLENEWTOOL_BONE_DEFAULT = BONE_DEFAULT + 5
 	
-	public static int SWORD_ADAMANT_DEFAULT = 5004;
-	public static int PICKAXE_ADAMANT_DEFAULT = 5005;
-	public static int SHOVEL_ADAMANT_DEFAULT = 5006;
-	public static int AXE_ADAMANT_DEFAULT = 5007;
+	public static int ADAMANT_DEFAULT = 5006;
+	public static int SWORD_ADAMANT_DEFAULT = ADAMANT_DEFAULT;
+	public static int PICKAXE_ADAMANT_DEFAULT = ADAMANT_DEFAULT + 1;
+	public static int SHOVEL_ADAMANT_DEFAULT = ADAMANT_DEFAULT + 2;
+	public static int AXE_ADAMANT_DEFAULT = ADAMANT_DEFAULT + 3;
+	public static int HAMMER_ADAMANT_DEFAULT = ADAMANT_DEFAULT + 4;
+	//public static int POSSIBLENEWTOOL_EMERALD_DEFAULT = ADAMANT_DEFAULT + 5
+	
+	
+	public static int HAMMER_DEFAULT = 5012;
+	public static int HAMMER_WOOD_DEFAULT = HAMMER_DEFAULT;
+	public static int HAMMER_STONE_DEFAULT = HAMMER_DEFAULT + 1;
+	public static int HAMMER_IRON_DEFAULT = HAMMER_DEFAULT + 2;
+	public static int HAMMER_GOLD_DEFAULT = HAMMER_DEFAULT + 3;
+	public static int HAMMER_DIAMOND_DEFAULT = HAMMER_DEFAULT + 4;
+
+	
 	
 	//ID
 	public static int BONE_INGOT;
@@ -26,10 +41,17 @@ public class ItemIDs
 	public static int PICKAXE_BONE;
 	public static int SHOVEL_BONE;
 	public static int AXE_BONE;
+	public static int HAMMER_BONE;
 	
 	public static int SWORD_ADAMANT;
 	public static int PICKAXE_ADAMANT;
 	public static int SHOVEL_ADAMANT;
 	public static int AXE_ADAMANT;
+	public static int HAMMER_ADAMANT;
 	
+	public static int HAMMER_WOOD;
+	public static int HAMMER_STONE;
+	public static int HAMMER_IRON;
+	public static int HAMMER_GOLD;
+	public static int HAMMER_DIAMOND;	
 }

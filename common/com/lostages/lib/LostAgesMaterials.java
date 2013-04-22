03ba579f7986304066f38b33fd7987f29d486e9e
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-04-22 15:52:04 +0200

    Added EnumToolMaterials

--- common/com/lostages/lib/LostAgesMaterials.java
+++ common/com/lostages/lib/LostAgesMaterials.java
@@ -5,7 +5,15 @@ import net.minecraftforge.common.EnumHelper;
 
 public class LostAgesMaterials
 {
+	//NAME(harvestlevel, uses, speed, damage, enchant)
 	
-	public static EnumToolMaterial BONE = EnumHelper.addToolMaterial("BONE", 1, 250, 3.0F, 1, 22);
-	
+    //WOOD(0, 59, 2.0F, 0, 15),
+    //STONE(1, 131, 4.0F, 1, 5),
+    //GOLD(0, 32, 12.0F, 0, 22);
+	public static EnumToolMaterial BONE = EnumHelper.addToolMaterial("BONE", 1, 200, 5.0F, 1, 20);
+	public static EnumToolMaterial BRONZE = EnumHelper.addToolMaterial("BRONZE", 2, 225, 6.0F, 2, 10);
+    //IRON(2, 250, 6.0F, 2, 14),
+	public static EnumToolMaterial STEEL = EnumHelper.addToolMaterial("STEEL", 2, 500, 7.0F, 3, 16);
+	public static EnumToolMaterial ADAMANT = EnumHelper.addToolMaterial("ADAMANT", 3, 1561, 8.0F, 3, 10);
+    //EMERALD(3, 1561, 8.0F, 3, 10),	
 }

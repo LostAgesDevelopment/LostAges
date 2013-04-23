67939fa3421f1428cbc394dfd38925e0a15cd933
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-04-23 18:43:06 +0200

    Made enumtools easier to read and understand with commented vanilla tools

--- common/com/lostages/lib/LostAgesMaterials.java
+++ common/com/lostages/lib/LostAgesMaterials.java
@@ -5,16 +5,16 @@ import net.minecraftforge.common.EnumHelper;
 
 public class LostAgesMaterials
 {
-	//NAME(harvestlevel, uses, speed, damage, enchant)
+																	  //NAME(harvestlevel, uses, speed, damage, enchant)
 	
-	//WOOD(0, 59, 2.0F, 0, 15),
-    //STONE(1, 131, 4.0F, 1, 5),
-    //GOLD(0, 32, 12.0F, 0, 22);
+																	 //WOOD(0, 59, 2.0F, 0, 15),
+    																 //STONE(1, 131, 4.0F, 1, 5),
+    																 //GOLD(0, 32, 12.0F, 0, 22);
 	public static EnumToolMaterial BONE = EnumHelper.addToolMaterial("BONE", 1, 200, 5.0F, 1, 20);
 	public static EnumToolMaterial BRONZE = EnumHelper.addToolMaterial("BRONZE", 2, 225, 6.0F, 2, 10);
-    //IRON(2, 250, 6.0F, 2, 14),
+    																  //IRON(2, 250, 6.0F, 2, 14),
 	public static EnumToolMaterial STEEL = EnumHelper.addToolMaterial("STEEL", 2, 500, 7.0F, 3, 16);
 	public static EnumToolMaterial STRONGGOLD = EnumHelper.addToolMaterial("STRONGGOLD", 3, 800, 12.0F, 3, 18);
 	public static EnumToolMaterial ADAMANT = EnumHelper.addToolMaterial("ADAMANT", 3, 1561, 8.0F, 3, 10);
-    //EMERALD(3, 1561, 8.0F, 3, 10),	
+    																	//EMERALD(3, 1561, 8.0F, 3, 10),	
 }
